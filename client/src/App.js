e3a89a6c4980af871702cca8dfbef9504816e262
Facuzanan88 <facundozanandrea@gmail.com>
2023-01-31 17:37:48 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 /* axios.defaults.baseURL = 'http://localhost:3001/' */
 
-axios.defaults.baseURL = 'pi-countries-production-c97b.up.railway.app'
+axios.defaults.baseURL = 'pi-countries-production-b25e.up.railway.app'
 
 function App() {
   return (
